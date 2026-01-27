16e84c9c1dd1ce891e10e896cb29f1555ed219c9
MS <schermulym@mail.de>
2026-01-27 23:13:13 +0100

    split languages, default en, add section The Brief

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/* change theme when available */
 function setStyle(name) {
   document.getElementById("theme").href = "styles/" + name + ".css";
+}
+
+/* change language */
+function setLang(lang) {
+  document.body.className = lang;
+  document.documentElement.lang = lang;
 }
