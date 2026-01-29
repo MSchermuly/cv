79ff2a641b039f8947ad530da7641b7579858090
MS <schermulym@mail.de>
2026-01-29 19:22:09 +0100

    add glass style

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 /* change theme when available */
 function setStyle(name) {
   document.getElementById("theme").href = "styles/" + name + ".css";
+  if (name == 'lean')
+    document.getElementById("profile_img").src = "img/profile_sw.jpg";
+  else
+    document.getElementById("profile_img").src = "img/profile.jpg";
 }
 
 /* change language */
